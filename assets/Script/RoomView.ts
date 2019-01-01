56aedb73302d55eb6dcb02a9d8ab0f3a279af6f0
ChengCheng <345199390@qq.com>
2019-01-02 02:05:39 +0800

    动画后切换场景

--- assets/Script/RoomView.ts
+++ assets/Script/RoomView.ts
@@ -8,6 +8,8 @@
 //  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
 //  - [English] http://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
 
+import delayTime = cc.delayTime;
+
 const {ccclass, property} = cc._decorator;
 
 @ccclass
@@ -22,9 +24,15 @@ export default class NewClass extends cc.Component {
     // @ts-ignore
     @property(cc.Node)
     btnRightNode: cc.Node = [];
+
+    @property(cc.Button)
+    btnStartGame: cc.Button = null;
+
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad() {
+
+    }
 
     start() {
     }
@@ -32,9 +40,9 @@ export default class NewClass extends cc.Component {
     onInRange() {
         for (let i = 0; i < this.btnRightNode.length; i++) {
             cc.log('now' + this.btnRightNode[i]);
-            let mTo = cc.moveTo(0.3,0,this.btnRightNode[i].y);
-            let dely = cc.delayTime(0.5*i);
-            let sqe = cc.sequence(dely,mTo);
+            let mTo = cc.moveTo(0.2, 0, this.btnRightNode[i].y);
+            let dely = cc.delayTime(0.5 * i);
+            let sqe = cc.sequence(dely, mTo);
             this.btnRightNode[i].runAction(sqe);
         }
     }
@@ -43,15 +51,19 @@ export default class NewClass extends cc.Component {
 
         for (let i = 0; i < this.btnRightNode.length; i++) {
             cc.log('now' + this.btnRightNode[i]);
-            let mTo = cc.moveTo(0.3,163,this.btnRightNode[i].y);
-            let dely = cc.delayTime(0.5*i);
-            let sqe = cc.sequence(dely,mTo);
+            let mTo = cc.moveTo(0.2, 163, this.btnRightNode[i].y);
+            let dely = cc.delayTime(0.3 * i);
+            let sqe = cc.sequence(dely, mTo);
             this.btnRightNode[i].runAction(sqe);
         }
     }
 
     // update (dt) {}
-    onSwitchScene() {  //切换场景
-        cc.director.loadScene("GameScene");
+    onGameStart() {  //切换场景
+        this.onOutRange();
+        setTimeout( function() {  //实现动画效果结束之后再切换场景
+            cc.director.loadScene("GameScene");
+
+        }, 800);
     }
 }
